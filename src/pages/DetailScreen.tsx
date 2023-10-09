2a81b5b1dfc936e6c11ca1c80bafcb8e549ad0d1
Jin <jpar303@gmail.com>
2023-10-10 01:08:55 +0900

    added: styled-component configuration

--- src/pages/DetailScreen.tsx
+++ src/pages/DetailScreen.tsx
@@ -1,28 +1,139 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-// eslint-disable-next-line import/no-unresolved
+import styled from 'styled-components';
 import { useStore } from '@components/zustand/jsonStore';
 import { convertFileSize, expirationTime } from '@components/utility/calculator';
 import { BsDownload } from 'react-icons/bs';
 import { DataItem, FileItem } from '@type/types';
-import {
-    StyledSection,
-    StyledHeader,
-    StyledColumntitle,
-    StyledSubSection2,
-    StyledSubSection2_1,
-    StyledSubSection2_btn,
-    StyledSubSection3,
-    StyledSubSection3_1,
-    StyledSubSection3_2,
-    StyledSubSection4,
-    StyledSubSection4_a,
-    StyledSubSection5,
-    StyledSubSection5a,
-    UnderLineText,
-    MainHeader,
-    StyledSubSection,
-} from '@components/styles/StyledComponentsDetail';
+
+export const StyledSection = styled.section`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    @media (min-width: 768px) {
+        margin: 50px;
+    }
+`;
+export const StyledHeader = styled.div`
+    width: 100%;
+    padding-top: 50px;
+    display: flex;
+    justify-content: center;
+`;
+export const StyledColumntitle = styled.div`
+    font-size: 16px;
+    font-weight: 600;
+    color: lightgray;
+    opacity: 0.8;
+`;
+
+export const StyledSubSection2 = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    border-bottom: 1px solid lightslategray;
+    border-left: 1px solid rgb(31, 41, 55);
+    border-right: 1px solid rgb(31, 41, 55);
+`;
+export const StyledSubSection2A = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+`;
+
+export const StyledSubSection2Btn = styled.button`
+    background-color: #1fd190;
+    width: 120px;
+    height: 50px;
+    border-radius: 10px;
+    cursor: pointer;
+`;
+
+export const StyledSubSection3 = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 20px 0;
+    width: 100%;
+`;
+export const StyledSubSection3A = styled.div`
+    display: flex;
+    width: 50%;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    padding: 10px;
+
+    width: 100%;
+`;
+
+export const StyledSubSection3B = styled.div`
+    display: flex;
+    width: 50%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+
+    width: 100%;
+`;
+
+export const StyledSubSection4 = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 40px;
+    border-top: 1px solid lightslategray;
+    border-bottom: 1px solid lightslategray;
+`;
+
+export const StyledSubSection4A = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 90%;
+`;
+
+export const StyledSubSection5 = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 0 10px 0;
+    width: 90%;
+`;
+export const StyledSubSection5A = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+export const UnderLineText = styled.div`
+    text-decoration: underline;
+`;
+
+export const MainHeader = styled.h1`
+    font-size: 18px;
+    font-weight: 600;
+`;
+
+export const StyledSubSection = styled.section`
+    width: 80%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-top: 0;
+    border-right: 1px solid lightslategray;
+    border-bottom: 1px solid lightslategray;
+    border-left: 1px solid lightslategray;
+`;
 
 function DetailScreen() {
     const { id } = useParams();
@@ -49,18 +160,18 @@ function DetailScreen() {
             </StyledHeader>
             <StyledSubSection>
                 <StyledSubSection2>
-                    <StyledSubSection2_1>
+                    <StyledSubSection2A>
                         <MainHeader>{filteredData[0]?.sent.subject}</MainHeader>
                         <UnderLineText>localhost/{filteredData[0].key}</UnderLineText>
-                    </StyledSubSection2_1>
-                    <StyledSubSection2_btn onClick={() => alert('downloaded')}>
+                    </StyledSubSection2A>
+                    <StyledSubSection2Btn onClick={() => alert('downloaded')}>
                         {' '}
                         <BsDownload /> Downlodad
-                    </StyledSubSection2_btn>
+                    </StyledSubSection2Btn>
                 </StyledSubSection2>
 
                 <StyledSubSection3>
-                    <StyledSubSection3_1>
+                    <StyledSubSection3A>
                         <div>
                             <StyledColumntitle>Link Creation Date</StyledColumntitle>
                             <div>
@@ -75,8 +186,8 @@ function DetailScreen() {
                             <StyledColumntitle>Download Count</StyledColumntitle>
                             <div>{filteredData[0].download_count}</div>
                         </div>
-                    </StyledSubSection3_1>
-                    <StyledSubSection3_2>
+                    </StyledSubSection3A>
+                    <StyledSubSection3B>
                         <img
                             style={{
                                 width: '50%',
@@ -87,11 +198,11 @@ function DetailScreen() {
                             src={filteredData[0].thumbnailUrl}
                             alt="thumbnail"
                         />
-                    </StyledSubSection3_2>
+                    </StyledSubSection3B>
                 </StyledSubSection3>
 
                 <StyledSubSection4>
-                    <StyledSubSection4_a>
+                    <StyledSubSection4A>
                         <StyledColumntitle>
                             Total {filteredData[0].download_count} file
                         </StyledColumntitle>
@@ -99,16 +210,17 @@ function DetailScreen() {
                             {' '}
                             {convertFileSize(filteredData[0].size)}
                         </StyledColumntitle>
-                    </StyledSubSection4_a>
+                    </StyledSubSection4A>
                 </StyledSubSection4>
                 <StyledSubSection5>
-                    <StyledSubSection5_1>
+                    <StyledSubSection5A>
                         <img
                             style={{ width: '10%', height: '10%', paddingRight: '15px' }}
                             src={fileData[0]?.thumbnailUrl}
+                            alt="sampleImage"
                         />
                         <div>{fileData[0]?.name}</div>
-                    </StyledSubSection5_1>
+                    </StyledSubSection5A>
                     <StyledColumntitle>
                         {checkExpiration ? <div>Expired</div> : convertFileSize(fileData[0]?.size)}
                     </StyledColumntitle>
--- src/pages/MainScreen.tsx
+++ src/pages/MainScreen.tsx
@@ -1,28 +1,100 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useStore } from '@components/zustand/jsonStore';
 import { CustomizedTable } from '@components/CustomizedTable';
 import { convertFileSize, expirationTime } from '@components/utility/calculator';
-import {
-    StyledSection,
-    StyledHeader,
-    StyledSubSection,
-    StyledPart2,
-    StyledSubTitle,
-} from '@components/styles/StyledComponents';
 import { FilteredItems, JsonData } from '@type/types';
 import styled from 'styled-components';
+import { device } from '@src/components/styles/BreakPoints';
+
+export const StyledSection = styled.section`
+    margin: 50px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const StyledHeader = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`;
+
+export const StyledSubTitle = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100%;
+    font-size: 22px;
+    font-weight: 600;
+`;
+
+export const StyledSubSection = styled.section`
+    width: 90%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const StyledPart2 = styled.div`
+    width: 100%;
+    height: 70%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+// For table component
+
+export const StyledRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const StyledSubRow = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding-left: 10px;
+`;
+export const StyledTable = styled.table`
+    width: 100%;
+    border-collapse: collapse;
+
+    th,
+    td {
+        padding: 10px;
+        border-bottom: 1px solid #ddd;
+    }
+`;
+
+export const CustomHeader = styled.th`
+    display: flex;
+    justify-content: flex-start;
+    border-bottom: none !important;
+`;
 
 const Title = styled.h1`
     color: ${(props) => props.theme.colors.yellow};
+    font-size: 40px;
+    @media ${device.md} {
+        font-size: 32px;
+    }
 `;
 
-function MainScreen() {
+function MainScreen(): React.JSX.Element {
     // Zustand 'useStore' hook to set data
     const setData = useStore((state) => state.setData);
     const [filtered, setFiltered] = useState<FilteredItems[]>([]);
 
     // get API data
-    async function fetchData() {
+    async function fetchData(): Promise<void> {
         try {
             const response = await fetch('/api/links.json');
             if (!response.ok) {
@@ -46,7 +118,7 @@ function MainScreen() {
             // Set the filtered Data
             setFiltered(filteredArray);
         } catch (e) {
-            console.error('Error:', e);
+            console.error('An error occurred:', e); // Changed: Added a more informative error message
         }
     }
 
@@ -59,7 +131,7 @@ function MainScreen() {
     return (
         <StyledSection>
             <StyledHeader>
-                <Title>Part. I</Title>
+                <Title>HAHA test</Title>
             </StyledHeader>
             <StyledSubSection>
                 <StyledPart2>
